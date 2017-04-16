68543ebea8da338349933262781886557a2011b5
marta95 <marta.czul@gmail.com>
2017-04-16 22:51:33 +0200

    Commit 3
    Pagination;
    Sorting, filtring optimization;

--- src/index.js
+++ src/index.js
@@ -1,41 +1,26 @@
 "use strict"
 
 var array = [];
-
+//reading Json file -> creating array of data
 function myFunction(callback){
     $.getJSON("sluzba.json", function(result){
         array = result;  
         callback(array);
-         
         tableMaker(array);
     });
-}
+};
 
 myFunction(function(myArray){
-   
-   filtFunction(myArray);
+   filtrFunction(myArray);
    sortId(myArray);
    sortExp(myArray);
    sortName(myArray);
    sortLastName(myArray);
    sortFunction(myArray);
    sortDate(myArray);
-   //fieldToggle();
-
-   var arr = [];
-   arr = parseHour(myArray[2].dateOfBirth);
-   console.log(arr);
-   var arr = [];
-   arr = parseHour(myArray[10].dateOfBirth);
-   console.log(arr);
-
-   var arr1 = [];
-   arr1 = parseDate(myArray[0].dateOfBirth);
-   console.log(arr1);
-
 });
 
-
+//creating a table and invoking pagination 
 function tableMaker(array){
   var table = document.getElementById('myTable').getElementsByTagName('tbody')[0];
     for(var i=0; i<array.length;i++)
@@ -54,123 +39,84 @@ function tableMaker(array){
             cell4.innerHTML = "<tr><td1>" +array[i].dateOfBirth+"</td1></tr>";
             cell5.innerHTML = "<tr><td1>" +array[i].function+"</td1></tr>";
             cell6.innerHTML = "<tr><td1>" +array[i].experience+"</td1></tr>";
-        } 
-    //pagination(array);
+        }
+        pagination(array);
 };
 
-//SORTWANIE
-function sortId(myArray){
+//removes rows with data
+function deleterow(){
+    var tableHeaderCount = 1;
+    var table = document.getElementById('myTable');
+    var rowCount = table.rows.length;
+    for (var i = tableHeaderCount; i < rowCount; i++){
+        table.deleteRow(tableHeaderCount);
+    }
+};
+
+//Invoking sorting functions ->creating new tables
+function sortId(array){
     document.getElementById("id").onclick = function()
    {
-       deleterow(arr1);
-       var arr=[];
-       arr = myArray;
-       //console.log(arr);
-       var arr1 = bubbleSortId(arr);
-       //console.log(arr1);
-       tableMaker(arr1);
+       deleterow();
+       var arr = bubbleSortId(array);
+       tableMaker(arr);
    }
 };
-
-function sortExp(myArray){
+function sortExp(array){
     document.getElementById("experience").onclick = function()
    {
-       deleterow(arr1);
-       var arr=[];
-       arr = myArray;
-       //console.log(arr);
-       var arr1 = bubbleSortExp(arr);
-       //console.log(arr1);
-       tableMaker(arr1);
+       deleterow();
+       var arr = bubbleSortExpAsc(array);
+       tableMaker(arr);
    }
 };
-
-function sortName(myArray){
+function sortName(array){
    document.getElementById("name").onclick = function()
    {
-       deleterow(arr1);
-       var arr=[];
-       arr = myArray;
-       //console.log(arr);
-       var arr1 = bubbleSortName(arr);
-      // console.log(arr1);
-       tableMaker(arr1);
+       deleterow();
+       var arr = bubbleSortName(array);
+       tableMaker(arr);
    } 
 };
-
-function sortLastName(myArray){
+function sortLastName(array){
    document.getElementById("lastName").onclick = function()
    {
-       deleterow(arr1);
-       var arr=[];
-       arr = myArray;
-       //console.log(arr);
-       var arr1 = bubbleSortSurname(arr);
-       //console.log(arr1);
+       deleterow();
+       var arr1 = bubbleSortSurname(array);
        tableMaker(arr1);
    } 
 };
-
-function sortFunction(myArray){
+function sortFunction(array){
    document.getElementById("function").onclick = function()
    {
-       deleterow(arr1);
-       var arr=[];
-       arr = myArray;
-       //console.log(arr);
-       var arr1 = bubbleSortFunction(arr);
-       //console.log(arr1);
-       tableMaker(arr1);
+       deleterow();
+       var arr = bubbleSortFunction(array);
+       tableMaker(arr);
    } 
 };
-
-function sortDate(myArray){
+function sortDate(array){
    document.getElementById("birthDate").onclick = function()
    {
-       deleterow(arr1);
-       var arr=[];
-       arr = myArray;
-       //console.log(arr);
-       var arr1 = bubbleSortDate(arr);
-       //console.log(arr1);
-       tableMaker(arr1);
+       deleterow();
+       var arr = bubbleSortDate(array);
+       tableMaker(arr);
    } 
 };
-
 function sortHour(myArray){
    document.getElementById("hourPicker").onclick = function()
    {
-       deleterow(arr1);
-       var arr=[];
-       arr = myArray;
-       //console.log(arr);
-       var arr1 = bubbleSortHour(arr);
-       //console.log(arr1);
-       tableMaker(arr1);
+       deleterow();
+       var arr = bubbleSortHour(array);
+       tableMaker(arr);
    } 
 };
 
-
-
-function deleterow(array){
-    var tableHeaderRowCount = 1;
-    var table = document.getElementById('myTable');
-    var rowCount = table.rows.length;
-    for (var i = tableHeaderRowCount; i < rowCount; i++) {
-        table.deleteRow(tableHeaderRowCount);
-}
-};
-
-
-//FUNKCJE SORTUJĄCE
+//sorting functions -> bubble sort for every attribute
 function bubbleSortId(array) {  
     var length = array.length;
      for (var i = (length - 1); i > 0; i--) {
-        //Number of passes
         for (var j = (length - i); j > 0; j--) {
-            //Compare the adjacent positions
             if (array[j].id < array[j - 1].id) {
-                //Swap the numbers
                 var tmp = array[j];
                 array[j] = array[j - 1];
                 array[j - 1] = tmp;
@@ -178,16 +124,12 @@ function bubbleSortId(array) {
         }
     }
     return array;
-}
-
-function bubbleSortExp(array) {  
+};
+function bubbleSortExpAsc(array) {  
     var length = array.length;
     for (var i = (length - 1); i > 0; i--) {
-        //Number of passes
         for (var j = (length - i); j > 0; j--) {
-            //Compare the adjacent positions
             if (array[j].experience < array[j - 1].experience) {
-                //Swap the numbers
                 var tmp = array[j];
                 array[j] = array[j - 1];
                 array[j - 1] = tmp;
@@ -195,17 +137,25 @@ function bubbleSortExp(array) {
         }
     }
     return array;
-}   
-
+};   
+function bubbleSortExpDesc(array) {  
+    var length = array.length;
+    for (var i = (length - 1); i > 0; i--) {
+        for (var j = (length - i); j > 0; j--) {
+            if (array[j].experience > array[j - 1].experience) {
+                var tmp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = tmp;
+            }
+        }
+    }
+    return array;
+};
 function bubbleSortName(array) {  
     var length = array.length;
-    //debugger;
     for (var i = (length - 1); i > 0; i--) {
-        //Number of passes
         for (var j = (length - i); j > 0; j--) {
-            //Compare the adjacent positions
             if (array[j].firstName.toLowerCase() < array[j - 1].firstName.toLowerCase()) {
-                //Swap the numbers
                 var tmp = array[j];
                 array[j] = array[j - 1];
                 array[j - 1] = tmp;
@@ -213,17 +163,12 @@ function bubbleSortName(array) {
         }
     }
     return array;
-} 
-
+}; 
 function bubbleSortSurname(array) {  
     var length = array.length;
-    //debugger;
     for (var i = (length - 1); i > 0; i--) {
-        //Number of passes
         for (var j = (length - i); j > 0; j--) {
-            //Compare the adjacent positions
             if (array[j].lastName.toLowerCase() < array[j - 1].lastName.toLowerCase()) {
-                //Swap the numbers
                 var tmp = array[j];
                 array[j] = array[j - 1];
                 array[j - 1] = tmp;
@@ -231,17 +176,12 @@ function bubbleSortSurname(array) {
         }
     }
     return array;
-} 
-
+}; 
 function bubbleSortFunction(array) {  
     var length = array.length;
-    //debugger;
     for (var i = (length - 1); i > 0; i--) {
-        //Number of passes
         for (var j = (length - i); j > 0; j--) {
-            //Compare the adjacent positions
             if (array[j].function.toLowerCase() < array[j - 1].function.toLowerCase()) {
-                //Swap the numbers
                 var tmp = array[j];
                 array[j] = array[j - 1];
                 array[j - 1] = tmp;
@@ -249,64 +189,27 @@ function bubbleSortFunction(array) {
         }
     }
     return array;
-} 
+};
 
-
-
-function parseDate(dateStr) {
-    var date = dateStr;
+//function for data parsing
+function parseDate(dateString) {
+    var date = dateString;
     var temp = String(date).indexOf(' ');
     date = date.substring(0, temp != -1 ? temp : date.length);
     date = String(date).split('.');
-    return date;
+    var day = date[0];var month = date[1];var year = date[2];
+    //created array -> into Date format
+    return new Date(year, month, day);
 };
-
 function bubbleSortDate(array) {  
     var length = array.length;
     var date = [];
     var date1 = [];
      for (var i = (length - 1); i > 0; i--) {
-        //Number of passes
         for (var j = (length - i); j > 0; j--) {
-            //Compare the adjacent positions
             date = parseDate(array[j].dateOfBirth);
             date1 = parseDate(array[j-1].dateOfBirth);
-            if (date[2] < date1[2] || 
-               (date[2]==date1[2]&& date[1]<date1[1]) || 
-               (date[2]==date1[2]&& date[1]==date1[1]&&date[0]<date1[0]))
-            {
-                //Swap the numbers
-                var tmp = array[j];
-                array[j] = array[j-1];
-                array[j-1]= tmp;
-            }
-        }
-    }
-    return array;
-}
-
-function parseHour(hourStr) {
-    var hour = hourStr;
-    var temp = String(hour).indexOf(' ');
-    hour = hour.substring(temp+1, temp!=-1 ? hour.length : hour.length-1);
-    hour = String(hour).split(':');
-    return hour;
-};
-
-function bubbleSortHour(array) {  
-    var length = array.length;
-    var hour = [];
-    var hour1 = [];
-     for (var i = (length - 1); i > 0; i--) {
-        //Number of passes
-        for (var j = (length - i); j > 0; j--) {
-            //Compare the adjacent positions
-            hour = parseHour(array[j].dateOfBirth);
-            hour1 = parseHour(array[j-1].dateOfBirth);
-            if (hour[0] < hour1[0] ||
-                (hour[0] == hour1[0] && hour[1] < hour1[1]))
-            {
-                //Swap the numbers
+            if (date < date1){
                 var tmp = array[j];
                 array[j] = array[j-1];
                 array[j-1]= tmp;
@@ -316,22 +219,17 @@ function bubbleSortHour(array) {
     return array;
 };
 
-
-
-function filtFunction(array)
+//filtr functions for every atribute
+function filtrFunction(array)
 {
     document.getElementById("filtr").onclick = function(){
-    if(document.getElementById("select").value === "id")
+        var txt = document.getElementById("textField").value;
+        var txt1 =document.getElementById("textField1").value;
+        var arr = [];
+        if(document.getElementById("select").value === "id")
         {
-            var txt = document.getElementById("textField").value;
-            var txt1 =document.getElementById("textField1").value;
-            console.log(txt);
-            console.log(txt1);
-            var arr = [];
-            for(var i = 0; i<array.length;i++)
-            {
-                if(txt <= array[i].id && txt1 >= array[i].id)
-                {
+            for(var i = 0; i<array.length;i++){
+                if(txt <= array[i].id && txt1 >= array[i].id){
                     arr.push(array[i]);
                     deleterow(arr);
                     tableMaker(arr);
@@ -339,14 +237,8 @@ function filtFunction(array)
             }
         }else if(document.getElementById("select").value === "name")
         {
-            var txt = document.getElementById("textField").value;
-            console.log(txt);
-            var arr = [];
-            debugger;
-            for(var i = 0; i<array.length;i++)
-            {
-                if(txt === array[i].firstName)
-                {
+            for(var i = 0; i<array.length;i++){
+                if(txt === array[i].firstName){
                     arr.push(array[i]);
                     deleterow(arr);
                     tableMaker(arr);
@@ -354,13 +246,8 @@ function filtFunction(array)
             }
         }else if(document.getElementById("select").value === "surname")
         {
-            var txt = document.getElementById("textField").value;
-            console.log(txt);
-            var arr = [];
-            for(var i = 0; i<array.length;i++)
-            {
-                if(txt === array[i].lastName)
-                {
+            for(var i = 0; i<array.length;i++){
+                if(txt === array[i].lastName){
                     arr.push(array[i]);
                     deleterow(arr);
                     tableMaker(arr);
@@ -368,13 +255,9 @@ function filtFunction(array)
             }
         }else if(document.getElementById("select").value === "function")
         {
-            var txt = document.getElementById("textField").value;
-            console.log(txt);
-            var arr = [];
             for(var i = 0; i<array.length;i++)
             {
-                if(txt === array[i].function)
-                {
+                if(txt === array[i].function){
                     arr.push(array[i]);
                     deleterow(arr);
                     tableMaker(arr);
@@ -382,15 +265,9 @@ function filtFunction(array)
             }
         }else if (document.getElementById("select").value === "experience")
         {
-            var txt = document.getElementById("textField").value;
-            var txt1 =document.getElementById("textField1").value;
-            console.log(txt);
-            console.log(txt1);
-            var arr = [];
             for(var i = 0; i<array.length;i++)
             {
-                if(txt <= array[i].experience && txt1 >= array[i].experience)
-                {
+                if(txt <= array[i].experience && txt1 >= array[i].experience){
                     arr.push(array[i]);
                     deleterow(arr);
                     tableMaker(arr);
@@ -398,24 +275,56 @@ function filtFunction(array)
             }
         }else if (document.getElementById("select").value === "birthdate")
         {
-            var txt = document.getElementById("textField").value;
-            var txt1 =document.getElementById("textField1").value;
-            console.log(txt);
-            console.log(txt1);
-            
-            var arr = [];
-            var date = [];
-            var date1 = [];
+            var dateTxt = parseDate(String(txt));
+            var dateTxt1 = parseDate(String(txt1));
             for (var j = (array.length-1); j >= 0; j--) 
             {
-                date = parseDate(array[j].dateOfBirth);
-                if(txt <= date[2] && txt1 >= date[2])
-                {
+                var date = parseDate(array[j].dateOfBirth);
+                if(dateTxt <= date && dateTxt1 >= date){
                     arr.push(array[j]);
                     deleterow(arr);
-                    tableMaker(arr);
+                        tableMaker(arr);
                 }
-            }
-        }
+            }      
+        } 
     };
-}
+};
+
+//pagination function
+function pagination(array){
+$('table.paginated').each(function() {
+    
+    var current = 0;
+    var numPerPage = 5; //number of displayed objects
+    var $table = $(this);
+
+    //hides all rows -> displays only the current sliced group
+    $table.bind('pagination', function() {
+        $table.find('tbody tr').hide().slice(current * numPerPage, (current + 1) * numPerPage).show();
+    });
+
+    $table.trigger('pagination');
+    //removes pagination buttons before making new ones
+    var pager;
+    if(pager = document.getElementsByClassName("pager"))
+    {
+        $('#pager').find('span').remove();
+    } 
+    //counting number of pages 
+    var numRows = $table.find('tbody tr').length;
+    var numPages = Math.ceil(numRows / numPerPage);
+
+    //loop for creating all the paggination buttons
+    var $pager = $('<div class="pager" id="pager"></div>');
+    for (var page = 0; page < numPages; page++) {
+        $('<span class="pageNumber"></span>').text(page + 1).bind('click', {
+            newPage: page }, function(event) {
+            current = event.data['newPage'];
+            $table.trigger('pagination');
+            $(this).addClass('active').siblings().removeClass('active');
+        }).appendTo($pager).addClass('clickable');
+    };
+    //insert newly made buttons under the table
+    $pager.insertAfter($table).find('span.pageNumber:first').addClass('active');
+});
+};
